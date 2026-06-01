55ab9299b00548ec3a55a9670c00a1f28b1e7926
DefazFebby <febbidefaz@gmail.com>
2026-06-01 13:48:38 +0700

    revisi side bar dan app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,13 @@
 @extends('adminlte::page')
 
 @section('content')
-<div id="main-content">
     @yield('page-content')
-</div>
+@stop
+
+@section('css')
+    @yield('css')
 @stop
 
 @section('js')
-<script>
-$(document).on('click', '.menu-link', function(e) {
-    e.preventDefault();
-
-    let url = $(this).attr('href');
-
-    $('#main-content').html(`
-        <div class="p-4 text-center">
-            <i class="fas fa-spinner fa-spin"></i> Memuat data...
-        </div>
-    `);
-
-    $('#main-content').load(url + ' #main-content > *', function() {
-        $('.nav-link').removeClass('active');
-        $('a[href="' + url + '"]').addClass('active');
-    });
-
-    history.pushState(null, '', url);
-});
-
-window.onpopstate = function() {
-    $('#main-content').load(location.href + ' #main-content > *');
-};
-</script>
+    @yield('js')
 @stop
--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -1,31 +1,19 @@
 <aside class="main-sidebar sidebar-dark-info elevation-4">
 
-    <!-- Logo -->
-    <a href="/dashboard" class="brand-link">
-
+    <a href="{{ route('rawatinap.index') }}" class="brand-link">
         <img src="{{ asset('vendor/adminlte/dist/img/AdminLTELogo.png') }}" alt="RSA"
             class="brand-image img-circle elevation-3" style="opacity:.8">
 
         <span class="brand-text font-weight-light">
             Billing RSA
         </span>
-
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
 
-        <!-- User Panel -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-
             <div class="img-circle elevation-2 d-flex align-items-center justify-content-center"
-                style="
-               width:35px;
-               height:35px;
-               background:#17a2b8;
-               color:white;
-               font-size:18px;
-            ">
+                style="width:35px;height:35px;background:#17a2b8;color:white;font-size:18px;">
                 <i class="fas fa-hospital-user"></i>
             </div>
 
@@ -34,60 +22,54 @@
                     {{ auth()->user()->Nama ?? 'Administrator' }}
                 </a>
             </div>
-
         </div>
 
-        <!-- Menu -->
         <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
 
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
-
-                <!-- Rawat Inap -->
                 <li class="nav-item">
-                    <a href="{{ route('rawatinap.index') }}" class="nav-link menu-link">
+                    <a href="{{ route('rawatinap.index') }}"
+                        class="nav-link {{ request()->routeIs('rawatinap.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-bed"></i>
                         <p>Rawat Inap</p>
                     </a>
                 </li>
 
-                <!-- Pulang -->
                 <li class="nav-item">
-                    <a href="{{ route('pulang.index') }}" class="nav-link menu-link">
+                    <a href="{{ route('pulang.index') }}"
+                        class="nav-link {{ request()->routeIs('pulang.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-home"></i>
                         <p>Pulang</p>
                     </a>
                 </li>
 
-                <!-- IGD -->
                 <li class="nav-item">
-                    <a href="{{ route('igd.index') }}" class="nav-link menu-link">
+                    <a href="{{ route('igd.index') }}"
+                        class="nav-link {{ request()->routeIs('igd.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-ambulance"></i>
                         <p>IGD</p>
                     </a>
                 </li>
 
-                <!-- Rawat Jalan -->
                 <li class="nav-item">
-                    <a href="{{ route('rawatjalan.index') }}" class="nav-link menu-link">
+                    <a href="{{ route('rawatjalan.index') }}"
+                        class="nav-link {{ request()->routeIs('rawatjalan.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-user-injured"></i>
                         <p>Rawat Jalan</p>
                     </a>
                 </li>
 
-                <!-- User Billing -->
-                @if (auth()->user()->Role == 'admin')
+                @if (auth()->user() && auth()->user()->Role == 'admin')
                     <li class="nav-item">
-                        <a href="{{ route('userbilling.index') }}" class="nav-link menu-link">
+                        <a href="{{ route('userbilling.index') }}"
+                            class="nav-link {{ request()->routeIs('userbilling.*') ? 'active' : '' }}">
                             <i class="nav-icon fas fa-users-cog"></i>
                             <p>User Billing</p>
                         </a>
                     </li>
                 @endif
 
-
             </ul>
-
         </nav>
 
     </div>
