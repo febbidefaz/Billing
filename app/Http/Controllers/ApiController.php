20eb27e16a5eaca88ae08860818c6d21d7ee2be3
DefazFebby <febbidefaz@gmail.com>
2026-09-23 09:06:38 +0700

    Bug Ambil Token Obapay API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -400,7 +400,7 @@ class ApiController extends Controller
     {
         return Cache::remember('farmasi_token', 360, function () {
 
-            $response = Http::post('http://192.168.3.31.9:8010/api/token', [
+            $response = Http::post('http://192.168.3.31:8010/api/token', [
                 'username' => env('FARMASI_USER'),
                 'password' => env('FARMASI_PASS')
             ]);
